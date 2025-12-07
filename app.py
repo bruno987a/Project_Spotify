d7eda86ddaa673b9bea1e0b292e9646c1f23c549
bruno987a <bruno.julienchambon@gmail.com>
2025-12-07 23:30:46 +0100

    test

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ st.markdown(
     <style>
     /* Make global text bigger */
     html, body, .stApp {
-        font-size: 18px;  /* base size up from default */
+        font-size: 10px;  /* base size up from default */
     }
 
     /* Overall background – white */
