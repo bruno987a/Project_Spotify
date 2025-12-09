8e45c2d1ee7c3fc7b69a3f801740f3841c42b338
bruno987a <bruno.julienchambon@gmail.com>
2025-12-09 19:01:13 +0100

    s

--- app.py
+++ app.py
@@ -781,7 +781,7 @@ if st.session_state.step >= 4 and st.session_state.evaluation_done:
                 )
 
                 ax2.set_xlim(0, 5)  # ratings from 1 to 5
-                ax2.set_xticks([1, 2, 3, 4, 5])
+                ax2.set_xticks([0, 1, 2, 3, 4, 5])
                 ax2.set_yticks(y_pos)
                 ax2.set_yticklabels(avg_labels)
                 ax2.set_xlabel("Average rating")
