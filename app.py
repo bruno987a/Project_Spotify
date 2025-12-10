aec6368f89f5faaae8bc5e0cfe16b55a89011711
emet67 <ericrene.mettler@student.unisg.ch>
2025-12-10 09:32:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -330,7 +330,7 @@ if st.session_state.step >= 2:
         # BEFORE confirming → show full criteria form
         if st.session_state.step == 2:
 
-            col1 = st.columns(1)
+            col1, = st.columns(1)
 
             with col1:
                 genre_map = {
@@ -357,7 +357,7 @@ if st.session_state.step >= 2:
 
             # Confirm button
             if st.button("✅ Confirm criteria & start rating", use_container_width=True):
-                if similarity_raw is None or genre_raw is None:
+                if genre_raw is None:
                     # "Popup" style warning 
                     st.markdown(
                         """
@@ -370,13 +370,12 @@ if st.session_state.step >= 2:
                             font-weight: 500;
                             margin-top: 0.5rem;
                             text-align: center;">
-                            Please choose both a similarity level and a preferred genre before continuing.
+                            Please choose a preferred genre before continuing.
                         </div>
                         """,
                         unsafe_allow_html=True,
                     )
                 else:
-                    st.session_state["similarity"] = similarity_raw
                     st.session_state.chosen_genre = genre_map[genre_raw]
 
                     st.session_state.criteria_confirmed = True
